1ca150b47535ff2c384531dbff1aebe5cfefa604
vikrant-ku <vikrant.rohartech@gmail.com>
2021-06-06 17:35:21 +0530

    change allow hosts

--- eCommerce/eCommerce/settings/dev.py
+++ eCommerce/eCommerce/settings/dev.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['naveenstores.in', 'www.naveenstores.in', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
--- eCommerce/eCommerce/settings/prod.py
+++ eCommerce/eCommerce/settings/prod.py
@@ -2,4 +2,4 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['naveenstores.in', 'www.naveenstores.in', '127.0.0.1']
+ALLOWED_HOSTS = ['naveenstores.in', 'www.naveenstores.in', ]
